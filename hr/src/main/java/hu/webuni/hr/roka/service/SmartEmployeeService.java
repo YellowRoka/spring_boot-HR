3896b9b9131d2e0a28334713b375788a6ed4a88e
fonad <fonad@LAPTOP-3JIULIRQ>
2021-03-28 16:55:58 +0200

    bugfix: correcting bad limit calculates
    new: limes and percent relocate into config. file

--- hr/src/main/java/hu/webuni/hr/roka/service/SmartEmployeeService.java
+++ hr/src/main/java/hu/webuni/hr/roka/service/SmartEmployeeService.java
@@ -2,6 +2,7 @@ package hu.webuni.hr.roka.service;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import hu.webuni.hr.roka.Employer;
@@ -9,19 +10,44 @@ import hu.webuni.hr.roka.Employer;
 @Service
 public class SmartEmployeeService implements EmployeeService
 {
+	
+	@Value("${hr.employment.junior.limit}")
+	private double juniorLimit;
+	@Value("${hr.employment.junior.paymentrise}")
+	private double juniorRise;
+	
+	@Value("${hr.employment.medior.limit}")
+	private double mediorLimit;
+	@Value("${hr.employment.medior.paymentrise}")
+	private double mediorRise;
+	
+	@Value("${hr.employment.senior.limit}")
+	private double seniorLimit;
+	@Value("${hr.employment.senior.paymentrise}")
+	private double seniorRise;
+	
+	@Value("${hr.employment.ceo.limit}")
+	private double ceoLimit;
+	@Value("${hr.employment.ceo.paymentrise}")
+	private double ceoRise;
 
 	@Override
 	public int getPayRaisePercent(Employer employer) {
-		int actTime = LocalDateTime.now().getYear();
-		int employeTime = employer.getFirstDate().getYear();
-		int calculatedTime = actTime-employeTime;
-		double risePercent = 0;
+		int actYear       = LocalDateTime.now().getYear();
+		int employeYear   = employer.getFirstDate().getYear();
+		int actMounth     = LocalDateTime.now().getMonthValue();
+		int employeMounth = employer.getFirstDate().getMonthValue();
+		
+		double calculatedTime = (actYear-employeYear) + ((double)(12-employeMounth+actMounth)/12.0);
+		double risePercent    = 0;
+		
 
-		if     ( 10  <= calculatedTime)                         risePercent = 0.10;
-		else if( 5   <= calculatedTime && 10 > calculatedTime ) risePercent = 0.05;
-		else if( 2.5 <= calculatedTime && 5  > calculatedTime)  risePercent = 0.02;
-		else if( 2    > calculatedTime)                         risePercent = 0.0;
-		else                                                    risePercent = 0.0;
+
+		if     ( ceoLimit      <= calculatedTime)                          risePercent = ceoRise;
+		else if( seniorLimit   <= calculatedTime && 10 > calculatedTime )  risePercent = seniorRise;
+		else if( mediorLimit   <= calculatedTime && 5  > calculatedTime )  risePercent = mediorRise;
+		else if( juniorLimit    > calculatedTime)                          risePercent = juniorRise;
+		else                                                               risePercent = 0.0;
 		
 		return (int)((employer.getPayment()*(1+risePercent)));
 	}
